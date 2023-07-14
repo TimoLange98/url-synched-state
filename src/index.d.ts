f184dd660464f1d266f01478778231b5b563b9b7
Timo Lange <timo.lange98@aol.de>
2023-07-14 16:01:26 +0200

    Update type definition

--- src/index.d.ts
+++ src/index.d.ts
@@ -2,4 +2,4 @@
  * Synchronizes the given state with the url-search-params
  * @param initial Initial state
  */
-export declare function useUrlSynchedState<T extends Record<string, any>>(initial: T): [T, <K extends keyof T>(key: K, newVal: string) => void]
+export declare function useUrlSynchedState<T extends Record<string, string | number | Array<string | number>>>(initial: T): [T, <K extends keyof T>(key: K, newVal: string) => void]
